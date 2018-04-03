1475fa15156503b947aea15f1db3b9b48c11b0a1
ericGOD24 <ericobandeira@hotmail.com>
2018-04-03 00:55:29 -0300

    resolução da parte a) do problema 1

--- problema1.c
+++ problema1.c
@@ -1,5 +1,5 @@
 /*
-  Érico Bandeira -
+  Érico Bandeira - 16/0070287
   Luciana Ribeiro Lins de Albuquerque - 15/0016131
   Max Henrique Barbosa - 16/0047013
 
@@ -7,8 +7,17 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define MAX 10
 
 int main() {
-
+  float ENTRADAS[MAX], PESOS[MAX], T;
+  int i;
+  printf("Digite 10 números reais: ");
+  for(i = 0; i < MAX; i++){
+    scanf("%f", &ENTRADAS[i]);
+  }
+  printf("Digite o valor do limiar T: ");
+  scanf("%f", T);
+  fneuronio();
   return 0;
 }
