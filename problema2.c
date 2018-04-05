50c08188fc7af14767dd565a75e323b4b614f4a5
lucianaribeiro <albuquerqueluciana54@gmail.com>
2018-04-05 01:22:45 -0300

    Finishing problem2

--- problema2.c
+++ problema2.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define MAX 4
+#define MAX 10
 
 //Funções
 void recebe_notas (double *, int, int *);
@@ -24,13 +24,7 @@ printf("Digite a nota de %d alunos: \n", MAX);
 for(i = 0; i < MAX; i++){
   scanf("%lf", &NOTAS[i]);
   if(NOTAS[i] < 0){
-    while(NOTAS[i] < 0){
-      printf("Valor invalido. Insira novamente: \n");
-      scanf("%lf", &NOTAS[i]);
-    }
-  }
-  else if(NOTAS[i] > 10){
-    while(NOTAS[i] > 10) {
+    while(NOTAS[i] < 0 || NOTAS[i] > 10){
       printf("Valor invalido. Insira novamente: \n");
       scanf("%lf", &NOTAS[i]);
     }
@@ -41,16 +35,15 @@ for(i = 0; i < MAX; i++){
 recebe_notas(NOTAS, MAX, APR);
 conta_notas(APR, MAX, &aprovados, &reprovados);
 percent_aprov(&aprovados, &reprovados, &percentAprovados, &percentReprovados);
-printf("Aprovados: %d\n", aprovados);
+printf("\nAprovados: %d\n", aprovados);
 printf("Reprovados: %d\n", reprovados);
-printf("Percentagem de Aprovados: %d\% \n", percentAprovados);
-printf("Percentagem Reprovados: %d\% \n", percentReprovados);
+printf("Porcentagem de Aprovados: %d\% \n", percentAprovados);
+printf("Porcentagem Reprovados: %d\% \n", percentReprovados);
 
 if(percent_aprov(&aprovados, &reprovados, &percentAprovados, &percentReprovados))
-  printf("Mais da metade da turma foi aprovada! \n");
+  printf("Mais da metade da turma foi aprovada! \n\n");
 
   return 0;
-
 }
 
 //Funções again
