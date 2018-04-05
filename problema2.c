7645947950325ff8e462ca9e6c82e5869ad09fe1
Max <maxhb.df@gmail.com>
2018-04-04 22:34:44 -0300

    updating second problem

--- problema2.c
+++ problema2.c
@@ -7,33 +7,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#define Max 10
+#define MAX 10
 
-void recebe_notas (int *, int);
+void recebe_notas (double *, int *);
+void conta_notas (int);
 
 int main() {
-double NOTAS [MAX];
-int i, APR[]*;
+double NOTAS[MAX];
+int i, APR[MAX];
 
-printf("Digite 10 números de notas: \n");
+printf("Digite a nota de 10 alunos: \n");
 for(i = 0; i < MAX; i++){
   scanf("%lf", &NOTAS[i]);
 }
 
+recebe_notas(NOTAS, &APR);
+printf("%d  \n", APR);
+
   return 0;
 }
 
-void recebe_notas (double *NOTAS [MAX], int){
-  int APR [MAX];
+void recebe_notas (double *NOTAS, int *APR){
   int k;
 
   for(k = 0 ; k < MAX ; k++){
 
     if(NOTAS[k] >= 6){
-      *APR[k] = 1;
+      APR[k] = 1;
     }
     else{
-      *APR[k] = 0;
+      APR[k] = 0;
     }
   }
+}
+
+void conta_notas (int APR [MAX]){
+
 }
